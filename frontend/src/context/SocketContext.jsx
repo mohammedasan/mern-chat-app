ca82633024cc2774c3e8f1c339fa10018da84fd1
mohammedasan <mohammedasan1480@gmail.com>
2025-04-29 23:21:41 +0530

    Socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -13,7 +13,7 @@ export const SocketContextProvider=({children})=>{
     useEffect(()=>{
         if(authUser)
         {
-            const socket=io("http://localhost:8000",{
+            const socket=io("https://mern-chat-app-1-igpj.onrender.com/",{
                 query:{
                     userId:authUser._id,
                 },
