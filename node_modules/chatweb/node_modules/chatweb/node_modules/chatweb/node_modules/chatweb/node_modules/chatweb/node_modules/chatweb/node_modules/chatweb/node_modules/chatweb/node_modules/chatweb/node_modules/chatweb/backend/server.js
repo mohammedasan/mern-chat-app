d2ada9d9d314ed74b7f4a843ed28a57c98806c40
mohammedasan <mohammedasan1480@gmail.com>
2025-06-13 21:56:41 +0530

    final20

--- node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/backend/server.js
+++ node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/node_modules/chatweb/backend/server.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import messageRoutes from "./routes/messages.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import { Server } from "socket.io";
 import http from "http";
 import path from "path";
